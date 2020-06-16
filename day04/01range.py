92bf6ad98e0b35c04121576d94a5c00252f2d4ac
박인욱 <yopy0817@naver.com>
2020-06-17 02:55:50 +0900

    파이선 수업자료

---
+++ day04/01range.py
@@ -0,0 +1,15 @@
+#리스트 선언
+list1 = ['park', 'kim', 'lee']
+list2 = [1,2,3,4,5]
+print(list1)
+print(list2)
+
+##range는 0부터 순서대로 길이 -1 까지의 순서대로 값을 생성
+print( range(10) )
+print( range(0, 10) )
+print( range(0, 10, 2) )
+
+print( list(range(10) ))
+print( list(range(0, 10) ))
+print( list(range(0, 10, 2) ))
+
